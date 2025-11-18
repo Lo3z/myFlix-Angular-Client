f342d0b65861e5682656f1148a04cd930a81bc2f
Lo3z <carlosheins1@gmail.com>
2025-11-18 18:36:10 -0500

    server file update

--- server/server/angular-app-engine-manifest.mjs
+++ server/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: 'MyFlix-Angular-Client',
+  basePath: '/myFlix-Angular-Client',
   supportedLocales: {
   "en-US": ""
 },
--- server/server/angular-app-manifest.mjs
+++ server/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'MyFlix-Angular-Client',
+  baseHref: '/myFlix-Angular-Client/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 24632, hash: '3c90e6090926cf48795414ddb553d6dd16899ac91c104a6f6f0aa9811f5d1969', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17085, hash: '0d948143fabc19350e2a92c39aa7fab4f0988398fb0616e8b5f58c1abee43932', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 24634, hash: 'd0f540882d6173bb93a9372f330c091b427321d7354e8b8668db0331af4bb726', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17087, hash: '66122009f5e465140f2f8d1f44ce5d3e9641b8bb85b1002ca261c081787f9ec8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-DTTV3AOM.css': {size: 8100, hash: 'jHWbwFO0LXY', text: () => import('./assets-chunks/styles-DTTV3AOM_css.mjs').then(m => m.default)}
   },
 };
